3d331d600e5545bd7e4932c718a904862424cf2a
Rebecca Mark <rebecca.lmark@gmail.com>
2018-05-15 16:42:19 -0700

    Formatting section of statements text

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -3,7 +3,9 @@ object Main extends App {
   val lines = wordSalad.read()
   val tokens = wordSalad.tokenize(lines)
   val ngrams = wordSalad.ngram(2, tokens)
+//  ngrams.take(5).foreach(println)
   wordSalad.makeDictionary(ngrams)
 
-  println(wordSalad.makeStatement().mkString(" "))
+  println(wordSalad.makeSentence().mkString(" "))
+//  println(wordSalad.makeStatement().mkString(" "))
 }
